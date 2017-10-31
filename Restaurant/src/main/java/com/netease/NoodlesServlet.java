cc1af0216e523c762cfdf51f66bf6a9f5b0f471a
gannimei <tczhoulan@163.com>
2017-10-31 11:21:39 +0800

    NoodlesServlet Complete

--- Restaurant/src/main/java/com/netease/NoodlesServlet.java
+++ Restaurant/src/main/java/com/netease/NoodlesServlet.java
@@ -1,7 +1,25 @@
 package com.netease;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class NoodlesServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter writer = resp.getWriter();
+		String vegetable = req.getParameter("vegetable");
+		if(vegetable == null) {
+			vegetable = "Tomato";
+		}
+		writer.println("<html><body>");
+        writer.println("<h1> Noodles with " + vegetable + "</h1>"); 
+        writer.println("</body></html>");
+	}
 
 }
